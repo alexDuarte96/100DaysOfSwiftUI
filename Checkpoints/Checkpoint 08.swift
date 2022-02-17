7da7981c7e6175bd83821b490dc2be7bc631300c
Alejandro Duarte López <39140613+alexDuarte96@users.noreply.github.com>
2022-02-16 22:16:32 -0600

    Return the square root of an Int between 1-10,000 (Brute force).

---
+++ Checkpoints/Checkpoint 08.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+// Problem: Write a Function that accepts an integer from 1 to 10,000 and returns the integer Square Root of that number. You can't use 'sqrt()'.
+enum rootError:Error{
+    case outOfBounds, noRoot
+}
+
+func squareRoot(of number: Int) throws -> Int{
+    if number < 0 || number > 10_000 { throw rootError.outOfBounds }
+    
+    if number == 1{
+        return 1
+    }else{
+        for i in 1 ... Int(number/2){
+            if i*i == number {
+                return i
+            }
+        }
+    }
+    throw rootError.noRoot
+}
+
+do{
+    try print("Square Root: \(squareRoot(of: 9025))")
+} catch rootError.outOfBounds {
+    print("Sorry, value out of bounds. Try values between 1 and 10,000.")
+} catch rootError.noRoot {
+    print("No square root found.")
+}
