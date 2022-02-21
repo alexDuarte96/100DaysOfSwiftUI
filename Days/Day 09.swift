4aa519567819c5925e6ec85bc4f681923e68a906
Alejandro Duarte López <39140613+alexDuarte96@users.noreply.github.com>
2022-02-20 23:48:04 -0600

    Closures.

--- Days/Day 09.swift
+++ Days/Day 09.swift
@@ -1,20 +1,30 @@
 import Cocoa
+import os
+import Foundation
 
 // --------
 // Closures
 // --------
 // Closures let us asign a block of code to a variable and pass it around as such. It is a function with no name that can be assigned to a variable.
+// Closures are a way to store functionallity when you don't know what must be done.
 
-// Closure without parameters.
+// Closure with NO parameters.
 /*
  var closureName = {
     ... some code
  }
  */
 
-// Closure with parameters.
+// Closure with parameters and Return Type.
 /*
  var closureName = { (parameter:Type) -> Type in
+    ... some code
+ }
+ */
+
+// Closure with NO parameters and Return Type.
+/*
+ var closureName = { () -> Type in
     ... some code
  }
  */
@@ -43,7 +53,6 @@ let modifyPassword: (String) -> Bool = changePassword
 let isValid = modifyPassword("Hello")
 print(isValid)
 
-
 // We can use Closures to modify another Function behavior when we send it to it.
 // For example, we can modify the 'sorted()' function to sort an element first.
 func zeroFirst(first:Int, second:Int) -> Bool {
@@ -60,4 +69,57 @@ let myList:[Int] = [9,4,1,2,0,3,0,4,8,-1,-5,-12]
 let newList = myList.sorted(by: zeroFirst)
 print(newList)
 
-// Closures are a way to store functionallity when you don't know what must be done.
+// We can also add the Closure directly in the function call. The closure we send MUST comply with the expected behavior.
+let anotherList = myList.sorted(by: { (first: Int, second: Int) -> Bool in
+    if first == 0 {
+        return true
+    } else if second == 0 {
+        return false
+    }
+    return first < second
+})
+print(anotherList)
+
+// Because the Closure must match the expected behaviour, Swift can infer its Parameter and Return types so we can symplify our code.
+/*
+ let yetAnotherList = myList.sorted({ a,b in
+    ... Rest of code.
+ })
+*/
+
+// -----------------
+// Short Hand Syntax
+// -----------------
+// Because the Function only recieves one Closure, you can skip the function Arguments names ('by:') and also the Parameters that the Closure needs.
+// When we skip the Closure Parameter names, they'll be replaced to numerical names starting with '0', like: '$0', '$1', and so on.
+/*
+ let inverseSortedList = myList.sorted {
+    return $0 > $1
+ }
+*/
+
+// As the code of the Closure is just one line, we can skip 'Return' and be simplified even more to just an expression.
+let inverseSortedList = myList.sorted {$0 > $1}
+print(inverseSortedList)
+
+// Example with 'filter()' function.
+let names = ["Alex", "Brendan", "Daniel", "Fran", "Andy"]
+
+let startsWithA = names.filter{ word in
+    return word.hasPrefix("A")
+}
+print(startsWithA)
+
+let onlyA = names.filter{ $0.hasPrefix("A") }
+print(onlyA)
+
+// Example with 'map()' function.
+let upperCased = names.map { word in
+    return word.uppercased()
+}
+print(upperCased)
+
+let upperCasedNames = names.map{ $0.uppercased() }
+print(upperCasedNames)
+
+// Closures are Widely used in Swift and SwiftUI. And example is when you click a button, it will ask for a Closure to define what to do after clicking it.
