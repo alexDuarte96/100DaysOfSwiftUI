39af84dd9ae3fe140648cfd779117a67f5be5d9a
Alejandro Duarte López <39140613+alexDuarte96@users.noreply.github.com>
2022-03-06 23:37:04 -0600

    Classes.

---
+++ Days/Day 12.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+//------
+// Class
+//------
+// Classes are similar to Structs in the way that you can create a new Variable Type, with properties, methods, observers, access control and initializers.
+// However, they are different by having Inheritance, which means a Class can inherit all Properties and Methods for another Class. Also, Classes are sent By Reference, which means if you 'copy' a Class and modify that copy, you will change both the original and the copy.
+
